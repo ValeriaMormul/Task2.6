7cbae2b4b998f4894972ee3f2e07083df90d4b81
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-04 20:46:01 +0200

    Добавьте файлы проекта.

---
+++ Task2.6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Вкажіть вік: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        if (age >= 12 && age <= 18)
+        {
+            Console.WriteLine("Вказаний вік є підлітковим");
+        }
+        else
+        {
+            Console.WriteLine("Вказаний вік не є підлітковим");
+        }
+    }
+}
